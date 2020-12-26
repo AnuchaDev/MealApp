108d1dbf6f37eab9e2133275c99c71cd1d754698
AnuchaDev <anuchadev@gmail.com>
2020-12-26 12:49:06 +0700

    First commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,550 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+import './models/meal.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c11',
+    title: 'Thailand',
+    color: Colors.cyanAccent,
+  ),
+  Category(
+    id: 'c12',
+    title: 'Steak',
+    color: Colors.brown,
+  ),
+];
+
+const DUMMY_MEALS = const [
+  Meal(
+    id: 'm1',
+    categories: [
+      'c1',
+      'c2',
+    ],
+    title: 'Spaghetti with Tomato Sauce',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+    duration: 20,
+    ingredients: [
+      '4 Tomatoes',
+      '1 Tablespoon of Olive Oil',
+      '1 Onion',
+      '250g Spaghetti',
+      'Spices',
+      'Cheese (optional)'
+    ],
+    steps: [
+      'Cut the tomatoes and the onion into small pieces.',
+      'Boil some water - add salt to it once it boils.',
+      'Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.',
+      'In the meantime, heaten up some olive oil and add the cut onion.',
+      'After 2 minutes, add the tomato pieces, salt, pepper and your other spices.',
+      'The sauce will be done once the spaghetti are.',
+      'Feel free to add some cheese on top of the finished dish.'
+    ],
+    isGlutenFree: false,
+    isVegan: true,
+    isVegetarian: true,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm2',
+    categories: [
+      'c2',
+    ],
+    title: 'Toast Hawaii',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg',
+    duration: 10,
+    ingredients: [
+      '1 Slice White Bread',
+      '1 Slice Ham',
+      '1 Slice Pineapple',
+      '1-2 Slices of Cheese',
+      'Butter'
+    ],
+    steps: [
+      'Butter one side of the white bread',
+      'Layer ham, the pineapple and cheese on the white bread',
+      'Bake the toast for round about 10 minutes in the oven at 200°C'
+    ],
+    isGlutenFree: false,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm3',
+    categories: [
+      'c2',
+      'c3',
+    ],
+    title: 'Classic Hamburger',
+    affordability: Affordability.Pricey,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2014/10/23/18/05/burger-500054_1280.jpg',
+    duration: 45,
+    ingredients: [
+      '300g Cattle Hack',
+      '1 Tomato',
+      '1 Cucumber',
+      '1 Onion',
+      'Ketchup',
+      '2 Burger Buns'
+    ],
+    steps: [
+      'Form 2 patties',
+      'Fry the patties for c. 4 minutes on each side',
+      'Quickly fry the buns for c. 1 minute on each side',
+      'Bruch buns with ketchup',
+      'Serve burger with tomato, cucumber and onion'
+    ],
+    isGlutenFree: false,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm4',
+    categories: [
+      'c4',
+    ],
+    title: 'Wiener Schnitzel',
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Challenging,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2018/03/31/19/29/schnitzel-3279045_1280.jpg',
+    duration: 60,
+    ingredients: [
+      '8 Veal Cutlets',
+      '4 Eggs',
+      '200g Bread Crumbs',
+      '100g Flour',
+      '300ml Butter',
+      '100g Vegetable Oil',
+      'Salt',
+      'Lemon Slices'
+    ],
+    steps: [
+      'Tenderize the veal to about 2–4mm, and salt on both sides.',
+      'On a flat plate, stir the eggs briefly with a fork.',
+      'Lightly coat the cutlets in flour then dip into the egg, and finally, coat in breadcrumbs.',
+      'Heat the butter and oil in a large pan (allow the fat to get very hot) and fry the schnitzels until golden brown on both sides.',
+      'Make sure to toss the pan regularly so that the schnitzels are surrounded by oil and the crumbing becomes ‘fluffy’.',
+      'Remove, and drain on kitchen paper. Fry the parsley in the remaining oil and drain.',
+      'Place the schnitzels on awarmed plate and serve garnishedwith parsley and slices of lemon.'
+    ],
+    isGlutenFree: false,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm5',
+    categories: [
+      'c2'
+          'c5',
+      'c10',
+    ],
+    title: 'Salad with Smoked Salmon',
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2016/10/25/13/29/smoked-salmon-salad-1768890_1280.jpg',
+    duration: 15,
+    ingredients: [
+      'Arugula',
+      'Lamb\'s Lettuce',
+      'Parsley',
+      'Fennel',
+      '200g Smoked Salmon',
+      'Mustard',
+      'Balsamic Vinegar',
+      'Olive Oil',
+      'Salt and Pepper'
+    ],
+    steps: [
+      'Wash and cut salad and herbs',
+      'Dice the salmon',
+      'Process mustard, vinegar and olive oil into a dessing',
+      'Prepare the salad',
+      'Add salmon cubes and dressing'
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm6',
+    categories: [
+      'c6',
+      'c10',
+    ],
+    title: 'Delicious Orange Mousse',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Hard,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2017/05/01/05/18/pastry-2274750_1280.jpg',
+    duration: 240,
+    ingredients: [
+      '4 Sheets of Gelatine',
+      '150ml Orange Juice',
+      '80g Sugar',
+      '300g Yoghurt',
+      '200g Cream',
+      'Orange Peel',
+    ],
+    steps: [
+      'Dissolve gelatine in pot',
+      'Add orange juice and sugar',
+      'Take pot off the stove',
+      'Add 2 tablespoons of yoghurt',
+      'Stir gelatin under remaining yoghurt',
+      'Cool everything down in the refrigerator',
+      'Whip the cream and lift it under die orange mass',
+      'Cool down again for at least 4 hours',
+      'Serve with orange peel',
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm7',
+    categories: [
+      'c7',
+    ],
+    title: 'Pancakes',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2018/07/10/21/23/pancake-3529653_1280.jpg',
+    duration: 20,
+    ingredients: [
+      '1 1/2 Cups all-purpose Flour',
+      '3 1/2 Teaspoons Baking Powder',
+      '1 Teaspoon Salt',
+      '1 Tablespoon White Sugar',
+      '1 1/4 cups Milk',
+      '1 Egg',
+      '3 Tablespoons Butter, melted',
+    ],
+    steps: [
+      'In a large bowl, sift together the flour, baking powder, salt and sugar.',
+      'Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.',
+      'Heat a lightly oiled griddle or frying pan over medium high heat.',
+      'Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.'
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm8',
+    categories: [
+      'c8',
+    ],
+    title: 'Creamy Indian Chicken Curry',
+    affordability: Affordability.Pricey,
+    complexity: Complexity.Challenging,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2018/06/18/16/05/indian-food-3482749_1280.jpg',
+    duration: 35,
+    ingredients: [
+      '4 Chicken Breasts',
+      '1 Onion',
+      '2 Cloves of Garlic',
+      '1 Piece of Ginger',
+      '4 Tablespoons Almonds',
+      '1 Teaspoon Cayenne Pepper',
+      '500ml Coconut Milk',
+    ],
+    steps: [
+      'Slice and fry the chicken breast',
+      'Process onion, garlic and ginger into paste and sauté everything',
+      'Add spices and stir fry',
+      'Add chicken breast + 250ml of water and cook everything for 10 minutes',
+      'Add coconut milk',
+      'Serve with rice'
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm9',
+    categories: [
+      'c9',
+    ],
+    title: 'Chocolate Souffle',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Hard,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2014/08/07/21/07/souffle-412785_1280.jpg',
+    duration: 45,
+    ingredients: [
+      '1 Teaspoon melted Butter',
+      '2 Tablespoons white Sugar',
+      '2 Ounces 70% dark Chocolate, broken into pieces',
+      '1 Tablespoon Butter',
+      '1 Tablespoon all-purpose Flour',
+      '4 1/3 tablespoons cold Milk',
+      '1 Pinch Salt',
+      '1 Pinch Cayenne Pepper',
+      '1 Large Egg Yolk',
+      '2 Large Egg Whites',
+      '1 Pinch Cream of Tartar',
+      '1 Tablespoon white Sugar',
+    ],
+    steps: [
+      'Preheat oven to 190°C. Line a rimmed baking sheet with parchment paper.',
+      'Brush bottom and sides of 2 ramekins lightly with 1 teaspoon melted butter; cover bottom and sides right up to the rim.',
+      'Add 1 tablespoon white sugar to ramekins. Rotate ramekins until sugar coats all surfaces.',
+      'Place chocolate pieces in a metal mixing bowl.',
+      'Place bowl over a pan of about 3 cups hot water over low heat.',
+      'Melt 1 tablespoon butter in a skillet over medium heat. Sprinkle in flour. Whisk until flour is incorporated into butter and mixture thickens.',
+      'Whisk in cold milk until mixture becomes smooth and thickens. Transfer mixture to bowl with melted chocolate.',
+      'Add salt and cayenne pepper. Mix together thoroughly. Add egg yolk and mix to combine.',
+      'Leave bowl above the hot (not simmering) water to keep chocolate warm while you whip the egg whites.',
+      'Place 2 egg whites in a mixing bowl; add cream of tartar. Whisk until mixture begins to thicken and a drizzle from the whisk stays on the surface about 1 second before disappearing into the mix.',
+      'Add 1/3 of sugar and whisk in. Whisk in a bit more sugar about 15 seconds.',
+      'whisk in the rest of the sugar. Continue whisking until mixture is about as thick as shaving cream and holds soft peaks, 3 to 5 minutes.',
+      'Transfer a little less than half of egg whites to chocolate.',
+      'Mix until egg whites are thoroughly incorporated into the chocolate.',
+      'Add the rest of the egg whites; gently fold into the chocolate with a spatula, lifting from the bottom and folding over.',
+      'Stop mixing after the egg white disappears. Divide mixture between 2 prepared ramekins. Place ramekins on prepared baking sheet.',
+      'Bake in preheated oven until scuffles are puffed and have risen above the top of the rims, 12 to 15 minutes.',
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm10',
+    categories: [
+      'c2',
+      'c5',
+      'c10',
+    ],
+    title: 'Asparagus Salad with Cherry Tomatoes',
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2018/04/09/18/26/asparagus-3304997_1280.jpg',
+    duration: 30,
+    ingredients: [
+      'White and Green Asparagus',
+      '30g Pine Nuts',
+      '300g Cherry Tomatoes',
+      'Salad',
+      'Salt, Pepper and Olive Oil'
+    ],
+    steps: [
+      'Wash, peel and cut the asparagus',
+      'Cook in salted water',
+      'Salt and pepper the asparagus',
+      'Roast the pine nuts',
+      'Halve the tomatoes',
+      'Mix with asparagus, salad and dressing',
+      'Serve with Baguette'
+    ],
+    isGlutenFree: true,
+    isVegan: true,
+    isVegetarian: true,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm11',
+    categories: [
+      'c1',
+      'c2',
+    ],
+    title: 'Mushroom Risotto',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/reallyeasymushroomri_70240_16x9.jpg',
+    duration: 30,
+    ingredients: [
+      '1 Tbsp Olive Oil',
+      '1 Medium Onion (Chopped)',
+      '5 cups (480g) Cremini Mushrooms (Sliced)',
+      '1 Tbsp Crushed Garlic',
+      '1 and 1/2 cups (300g) Risotto (Arborio) Rice',
+      '6 cups Vegetable Stock (Divided)',
+      '2 Tbsp Vegan Butter',
+      'Sea Salt (to taste)',
+    ],
+    steps: [
+      'Soak the porcini mushrooms in hot water for 10 minutes, then drain well. Heat the oil in a large, heavy based saucepan and add the onion and garlic. Fry over a gentle heat for 2-3 minutes, until softened. Add the chestnut mushrooms and fry for a further 2-3 minutes, until browned.',
+      'Stir in the rice and coat in the oil. Pour in the wine and simmer, stirring, until the liquid has been absorbed. Add a ladleful of the stock and simmer, stirring again, until the liquid has been absorbed. Continue adding the stock in this way, until all the liquid has been absorbed and the rice is plump and tender.',
+      'Roughly chop the soaked porcini mushrooms and stir into the risotto, along with the parsley, butter and salt and pepper. Serve with freshly grated Parmesan',
+    ],
+    isGlutenFree: true,
+    isVegan: true,
+    isVegetarian: true,
+    isLactoseFree: true,
+  ),
+  Meal(
+    id: 'm12',
+    categories: [
+      'c9',
+    ],
+    title: 'Classic chocolate crepes',
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://www.thespruceeats.com/thmb/_JKsT3UQrd3W1w_ypi7JBLqPwK0=/3120x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/crepes-with-chocolate-sauce-110856358-57c5e9ef3df78cc16ed8c5ed.jpg',
+    duration: 20,
+    ingredients: [
+      '2 cups milk',
+      '1 1/2 cups flour',
+      '1/2 cup cocoa powder',
+      '6 tablespoons powdered sugar',
+      '2 large eggs',
+      '2 tablespoons butter (melted)',
+      '1/2 teaspoon pure vanilla extract',
+      '1/4 teaspoon salt​',
+    ],
+    steps: [
+      'Sift flour into a bowl. Mix in sugar. Make a well in centre of dry ingredients.',
+      'Combine in a bowl, milk and eggs to form a smooth batter (consistency of pouring cream). Whisk in chocolate. Gradually whisk in to dry ingredients.',
+      'Heat a small frying pan or crepe pan on medium. Brush with a little butter. Pour batter into pan, swirling to cover base. Cook for 1-2 minutes. Turn over and cook for 30 seconds.',
+      'Transfer to a plate. Continue with remaining batter, brushing pan with butter as required.',
+      'Serve crepes folded and topped with cream, berries, grated chocolate and chocolate sauce, if desired.'
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm13',
+    categories: [
+      'c11',
+    ],
+    title: 'Papaya salad',
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Hard,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2015/04/07/07/51/papaya-salad-710613_1280.jpg',
+    duration: 10,
+    ingredients: [
+      '5 Cherry Tomatoes',
+      '2 chili peppers',
+      '1 tablespoon dried shrimp',
+      '1 1/2 tablespoons fish sauce',
+      '1 clove garlic',
+      '6 green beans',
+      '2 cups shredded green papaya',
+      '3/4 lime',
+      '1 1/2 tablespoons Palm Sugar',
+      '2 tablespoons toasted peanuts Optional',
+    ],
+    steps: [
+      'In a blender or mortar, blend or pound garlic, salt, peanuts, chilies, sugar and shrimp (if using) into a paste. Transfer to a large bowl and mix in lime juice and fish sauce. Use a spoon (or the mortar) to lightly crush tomatoes and beans (if using), then add to bowl and mix lightly.',
+      'Peel and coarsely grate or shred papaya, discarding seeds and inner membrane. There should be 4 to 6 cups.',
+      'Add papaya to bowl and lightly but thoroughly toss together. Taste for seasoning. Mound in a bowl (if desired, line bowl with lettuce leaves beforehand). Sprinkle with peanuts and serve.'
+    ],
+    isGlutenFree: true,
+    isVegan: false,
+    isVegetarian: true,
+    isLactoseFree: false,
+  ),
+  Meal(
+    id: 'm14',
+    categories: [
+      'c12',
+    ],
+    title: 'Steak',
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Simple,
+    imageUrl:
+        'https://images.unsplash.com/photo-1546964124-0cce460f38ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+    duration: 30,
+    ingredients: [
+      '2 lbs New York Strip Steaks (2 steaks) or Ribeye or Top Sirloin Steaks (1 lb each steak, 1 1/4” thick)',
+      '1/2 Tbsp vegetable oil or any high heat cooking oil like canola or extra light olive oil',
+      '1 1/2 tsp sea salt',
+      '1 tsp black pepper freshly ground',
+      '2 Tbsp unsalted butter',
+      '2 cloves garlic peeled and quartered',
+    ],
+    steps: [
+      'Rub the steak all over with a good lug of olive oil and a good pinch of sea salt and black pepper',
+      'Add the steak to a hot pan, then cook for 6 minutes for medium-rare, or to your liking, turning every minute',
+      'For more flavour, try one or a combination of the following… ',
+      'Halve a garlic clove and rub it over the steak every time you turn it',
+      'Rub the steak with a knob of butter – the sweetness from the butter will make it taste divine!',
+      'Or create a herb brush by tying woody herbs like thyme or rosemary to the handle of a wooden spoon and brush it over the steak every minute or so',
+      'Once cooked to your liking, rest the steak on a plate that can collect all the lovely juices for 2 minutes',
+      'Carve with a nice sharp carving knife, then serve with the resting juices drizzled on top'
+    ],
+    isGlutenFree: false,
+    isVegan: false,
+    isVegetarian: false,
+    isLactoseFree: true,
+  ),
+];
